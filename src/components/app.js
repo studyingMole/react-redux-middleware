f54f452ab0972ce6b0c3d3e3da7e92357a51e1eb
CodeSchwert <studying.mole@gmail.com>
2019-05-15 13:17:41 +1200

    Less breaking stuff ...

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import UserList from './user_list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <UserList />
-      </div>
+      <div></div>
     );
   }
 }
